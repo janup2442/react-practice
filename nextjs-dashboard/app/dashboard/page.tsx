1b1c0b71d3d4af1dd6e290ced4f7ef909163b42c
Anup Jaiswal <janup2442@gmail.com>
2024-07-18 06:36:57 +0000

    basic front end done

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import SideNav from "../ui/dashboard/sidenav";
+
+export default function Page(){
+    return(
+        <>
+            
+            <div>this is a dashboard page</div>
+        </>
+    );
+}
